12174775d0d2ac36fc5709bcc6ed3fa6b70590cc
Jose Luis Cercos Pita <jlcercos@gmail.com>
2014-01-24 12:01:59 +0100

    Added basic controller

--- resources/sssEmit/scripts/emitter.py
+++ resources/sssEmit/scripts/emitter.py
@@ -17,3 +17,54 @@
 #  along with AQUAgpusph.  If not, see <http://www.gnu.org/licenses/>.       #
 #                                                                            #
 ##############################################################################
+
+import bge
+from bge import logic as g
+import mathutils
+import math
+import random
+
+
+def load():
+    """Method called one time at the emitter generation"""
+    cont = g.getCurrentController()
+    obj = cont.owner
+    obj['t'] = 0.0  # Emitter lifetime
+    obj['count'] = 0  # Number of particles emitted
+    return
+
+
+def generateParticle(obj):
+    """Generate a particle from the emitter"""
+    obj['count'] += 1
+    return
+
+
+def lifetime(obj):
+    """Test the object lifetime"""
+    if not obj['is_lifetime']:
+        return
+    if obj['t'] >= obj['lifetime']:
+        obj.endObject()
+
+
+def update():
+    """Method called each frame while the emitter exist"""
+    cont = g.getCurrentController()
+    obj = cont.owner
+    scene = bge.logic.getCurrentScene()
+    cam = scene.active_camera
+
+    # Targeted number of particles and remaining ones to achieve it
+    dt = 1.0/g.getLogicTicRate()
+    obj['t'] += dt
+    target = int(obj['t']*obj['rate'])
+    n = target - obj['count']
+
+    while(obj['count'] < n):
+        generateParticle(obj)
+
+    # Test if the object must end
+    lifetime(obj)
+
+    return
