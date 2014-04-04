333c16b88cb612019f4db92780dcdfcf85beeb4f
Jose Luis Cercos Pita <jlcercos@gmail.com>
2014-04-04 14:08:05 +0200

    Created a SDK draft to accelrate the entities development

---
+++ resources/sssSDK/objects/dynamic.py
@@ -0,0 +1,198 @@
+##############################################################################
+#                                                                            #
+#  This file is part of SonSilentSea, a free ships based combatr game.       #
+#  Copyright (C) 2014  Jose Luis Cercos Pita <jlcercos@gmail.com>            #
+#                                                                            #
+#  AQUAgpusph is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by      #
+#  the Free Software Foundation, either version 3 of the License, or         #
+#  (at your option) any later version.                                       #
+#                                                                            #
+#  AQUAgpusph is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#  GNU General Public License for more details.                              #
+#                                                                            #
+#  You should have received a copy of the GNU General Public License         #
+#  along with AQUAgpusph.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                            #
+##############################################################################
+
+import bpy
+from os import path
+
+
+# Module data
+NAME = 'Dynamic object'
+DESCRIPION = 'A dynamic object'
+CLASS_NAME = 'sssDynamic'
+SCRIPT_NAME = 'sss_dynamic'
+
+MASS_FACTOR = 1E-3
+
+
+def scriptPaths():
+    """Return all the possible locations for the scripts."""
+    paths = bpy.utils.script_paths(check_all=True)
+    paths.append(path.join(bpy.utils.resource_path('USER'), 'scripts'))
+    paths.append(path.join(bpy.utils.resource_path('LOCAL'), 'scripts'))
+    paths.append(path.join(bpy.utils.resource_path('SYSTEM'), 'scripts'))
+    return paths
+
+
+def addonsPaths():
+    """ Return all the possible locations for the addons """
+    paths = []
+    for folder in scriptPaths():
+        f = path.join(folder, 'addons')
+        if path.isdir(f):
+            paths.append(f)
+        f = path.join(folder, 'addons_extern')
+        if path.isdir(f):
+            paths.append(f)
+    return paths
+
+
+def addProperty(name, type_id, value):
+    """Test if a property exist in the object, adding it otherwise.
+
+    Keyword arguments:
+    name -- Property name
+    type_id -- Type of property
+    value -- Property value
+    """
+    obj = bpy.context.object
+    if not name in obj.game.properties.keys():
+        bpy.ops.object.game_property_new()
+        obj.game.properties[-1].name = name
+        obj.game.properties[name].type = type_id
+        obj.game.properties[name].value = value
+
+
+def delProperty(name):
+    """Remove a property from the object if it exist.
+
+    Keyword arguments:
+    name -- Property name
+    """
+    obj = bpy.context.object
+    if not name in obj.game.properties.keys():
+        return
+
+    for i, p in enumerate(obj.game.properties):
+        if p.name == name:
+            bpy.ops.object.game_property_remove(i)
+            return
+
+
+def generateProperties():
+    """Ensure that the object has the required properties."""
+    addProperty('t', 'TIMER', 0.0)
+    addProperty('real_mass', 'STRING', '1.0')
+
+
+def updateValues():
+    """Update the particles emitter values."""
+    generateProperties()
+    loadScript()
+
+    obj = bpy.context.object
+    obj.game.properties['real_mass'].value = obj.sss_mass
+    m = min(10000.0, max(0.01, MASS_FACTOR * float(obj.sss_mass)))
+    obj.game.mass = m
+
+
+def generateObjectProperties(update_callback):
+    """Generate the Blender object properties.
+    """
+    bpy.types.Object.sss_mass = bpy.props.StringProperty(
+        default='1.0',
+        update=update_callback,
+        description='Real mass of the object. It is needed due to the'
+                    ' restrictions imposed by Blender in the mass field')
+
+
+def loadScript():
+    """Load/update the text script in text editor."""
+    filepath = None
+    for folder in addonsPaths():
+        f = path.join(folder, "sssSDK/scripts/{}.py").format(SCRIPT_NAME)
+        if not path.isfile(f):
+            continue
+        filepath = f
+        break
+    if not filepath:
+        raise Exception(
+            'I can not find the script file "{}.py"'.format(SCRIPT_NAME))
+
+    # We can try to update it, and if the operation fails is just because the
+    # file has not been loaded yet
+    try:
+        text = bpy.data.texts['{}.py'.format(SCRIPT_NAME)]
+        text.clear()
+        f = open(filepath, 'r')
+        text.write(f.read())
+        f.close()
+    except:
+        bpy.ops.text.open(filepath=filepath,
+                          filter_blender=False,
+                          filter_image=False,
+                          filter_movie=False,
+                          filter_python=True,
+                          filter_font=False,
+                          filter_sound=False,
+                          filter_text=True,
+                          filter_btx=False,
+                          filter_collada=False,
+                          filter_folder=True,
+                          filemode=9,
+                          internal=True)
+
+
+def createLogic():
+    obj = bpy.context.active_object
+
+    # Add a controller to reference the script (but never used). It is
+    # useful if the object will be imported from other blender file,
+    # inserting the script in the importer scene
+    bpy.ops.logic.controller_add(type='PYTHON',
+                                 name="{}.reference".format(SCRIPT_NAME),
+                                 object="")
+    text = None
+    for t in bpy.data.texts:
+        if t.name == '{}.py'.format(SCRIPT_NAME):
+            text = t
+            break
+    if text is None:
+        raise Exception('The script "{}.py is not loaded"'.format(SCRIPT_NAME))
+    obj.game.controllers[-1].mode = 'SCRIPT'
+    obj.game.controllers[-1].text = text
+
+
+def createPhysics():
+    obj = bpy.context.active_object
+
+    obj.game.physics_type = 'RIGID_BODY'
+    mask = list(obj.game.collision_group)
+    mask[-1] = False
+    mask[-2] = False
+    obj.game.collision_group = mask
+    mask = list(obj.game.collision_mask)
+    mask[-1] = False
+    mask[-2] = False
+    obj.game.collision_mask = mask
+    obj.game.mass = MASS_FACTOR * float(obj.sss_mass)
+
+
+def create():
+    generateProperties()
+    loadScript()
+    createLogic()
+    createPhysics()
+
+
+def draw(context, layout):
+    row = layout.row()
+    row.prop(context.object,
+             "sss_mass",
+             text="mass (tons)")
---
+++ resources/sssSDK/scripts/sss_dynamic.py
@@ -0,0 +1,63 @@
+##############################################################################
+#                                                                            #
+#  This file is part of SonSilentSea, a free ships based combatr game.       #
+#  Copyright (C) 2014  Jose Luis Cercos Pita <jlcercos@gmail.com>            #
+#                                                                            #
+#  AQUAgpusph is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by      #
+#  the Free Software Foundation, either version 3 of the License, or         #
+#  (at your option) any later version.                                       #
+#                                                                            #
+#  AQUAgpusph is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#  GNU General Public License for more details.                              #
+#                                                                            #
+#  You should have received a copy of the GNU General Public License         #
+#  along with AQUAgpusph.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                            #
+##############################################################################
+
+import bge
+from bge import logic as g
+
+
+import bge
+from bge import logic as g
+from math import *
+from mathutils import *
+
+class sssDynamic(bge.types.KX_GameObject):
+    def __init__(self, obj):
+        self.v = None
+        self.vback = None
+
+    def update(self):
+        self.vback = self.v
+        self.v = self.getLinearVelocity()
+        if self.vback is None:
+            self.vback = self.v
+
+    def getLinearMomentum(self):
+        """Returns the linear momentum p = m * v. You may consider using the
+        kinetic energy instead, which is cheaper computed.
+        """
+        if self.vback is None:
+            return 0.0
+        m = self.mass
+        if 'real_mass' in self:
+            m = float(self['real_mass'])
+        v = self.v.length
+        return m * v
+
+    def getKineticEnergy(self):
+        """Returns the kinetic energy e = 1/2 m * v**2. It is cheaper to the
+        linear momentum computation.
+        """
+        if self.vback is None:
+            return 0.0
+        m = self.mass
+        if 'real_mass' in self:
+            m = float(self['real_mass'])
+        v2 = self.v.length_squared
+        return 0.5 * m * v2
