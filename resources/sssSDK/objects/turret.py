772221d9724b21553a65e02bb7ccf341a211d02b
Jose Luis Cercos Pita <jlcercos@gmail.com>
2014-06-20 11:29:40 +0200

    Added turret object

---
+++ resources/sssSDK/objects/turret.py
@@ -0,0 +1,217 @@
+##############################################################################
+#                                                                            #
+#  This file is part of SonSilentSea, a free ships based combatr game.       #
+#  Copyright (C) 2014  Jose Luis Cercos Pita <jlcercos@gmail.com>            #
+#                                                                            #
+#  AQUAgpusph is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by      #
+#  the Free Software Foundation, either version 3 of the License, or         #
+#  (at your option) any later version.                                       #
+#                                                                            #
+#  AQUAgpusph is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#  GNU General Public License for more details.                              #
+#                                                                            #
+#  You should have received a copy of the GNU General Public License         #
+#  along with AQUAgpusph.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                            #
+##############################################################################
+
+import bpy
+from os import path
+import math
+import sssSDK.objects.dynamic as dynamic
+import sssSDK.objects.destroyable as destroyable
+
+
+# Module data
+NAME = 'Turret'
+DESCRIPION = 'Turret. The Turret will rotate around the Z axis.'
+SELECTABLE = True
+CLASS_NAME = 'sssTurret'
+SCRIPT_NAME = 'sss_turret'
+
+
+def scriptPaths():
+    """Return all the possible locations for the scripts."""
+    paths = bpy.utils.script_paths(check_all=True)
+    paths.append(path.join(bpy.utils.resource_path('USER'), 'scripts'))
+    paths.append(path.join(bpy.utils.resource_path('LOCAL'), 'scripts'))
+    paths.append(path.join(bpy.utils.resource_path('SYSTEM'), 'scripts'))
+    return paths
+
+
+def addonsPaths():
+    """ Return all the possible locations for the addons """
+    paths = []
+    for folder in scriptPaths():
+        f = path.join(folder, 'addons')
+        if path.isdir(f):
+            paths.append(f)
+        f = path.join(folder, 'addons_extern')
+        if path.isdir(f):
+            paths.append(f)
+    return paths
+
+
+def addProperty(name, type_id, value):
+    """Test if a property exist in the object, adding it otherwise.
+
+    Keyword arguments:
+    name -- Property name
+    type_id -- Type of property
+    value -- Property value
+    """
+    obj = bpy.context.object
+    if not name in obj.game.properties.keys():
+        bpy.ops.object.game_property_new()
+        obj.game.properties[-1].name = name
+        obj.game.properties[name].type = type_id
+        obj.game.properties[name].value = value
+
+
+def delProperty(name):
+    """Remove a property from the object if it exist.
+
+    Keyword arguments:
+    name -- Property name
+    """
+    obj = bpy.context.object
+    if not name in obj.game.properties.keys():
+        return
+
+    for i, p in enumerate(obj.game.properties):
+        if p.name == name:
+            bpy.ops.object.game_property_remove(i)
+            return
+
+
+def generateProperties():
+    """Ensure that the object has the required properties."""
+    obj = bpy.context.object
+    addProperty('min_angle', 'FLOAT', -360.0)
+    addProperty('max_angle', 'FLOAT', 360.0)
+    addProperty('vel_angle', 'FLOAT', 5.0)
+
+
+def updateValues():
+    """Update the particles emitter values."""
+    dynamic.updateValues()
+    generateProperties()
+    loadScript()
+
+    obj = bpy.context.object
+    obj.game.properties['min_angle'].value = obj.sss_min_angle
+    obj.game.properties['max_angle'].value = obj.sss_max_angle
+    obj.game.properties['vel_angle'].value = obj.sss_vel_angle
+
+def generateObjectProperties(update_callback):
+    """Generate the Blender object properties.
+    """
+    bpy.types.Object.sss_min_angle = bpy.props.FloatProperty(
+        default=-360.0,
+        min=-360.0,
+        max=0.0,
+        update=update_callback,
+        description='Minimum rotation angle (-360 to no limit).')
+    bpy.types.Object.sss_max_angle = bpy.props.FloatProperty(
+        default=360.0,
+        min=0.0,
+        max=360.0,
+        update=update_callback,
+        description='Maximum rotation angle (360 to no limit).')
+    bpy.types.Object.sss_vel_angle = bpy.props.FloatProperty(
+        default=5.0,
+        min=1.0,
+        max=360.0,
+        update=update_callback,
+        description='Rotation velocity.')
+
+
+def loadScript():
+    """Load/update the text script in text editor."""
+    filepath = None
+    for folder in addonsPaths():
+        f = path.join(folder, "sssSDK/scripts/{}.py").format(SCRIPT_NAME)
+        if not path.isfile(f):
+            continue
+        filepath = f
+        break
+    if not filepath:
+        raise Exception(
+            'I can not find the script file "{}.py"'.format(SCRIPT_NAME))
+
+    # We can try to update it, and if the operation fails is just because the
+    # file has not been loaded yet
+    try:
+        text = bpy.data.texts['{}.py'.format(SCRIPT_NAME)]
+        text.clear()
+        f = open(filepath, 'r')
+        text.write(f.read())
+        f.close()
+    except:
+        bpy.ops.text.open(filepath=filepath,
+                          filter_blender=False,
+                          filter_image=False,
+                          filter_movie=False,
+                          filter_python=True,
+                          filter_font=False,
+                          filter_sound=False,
+                          filter_text=True,
+                          filter_btx=False,
+                          filter_collada=False,
+                          filter_folder=True,
+                          filemode=9,
+                          internal=True)
+
+
+def createLogic():
+    obj = bpy.context.active_object
+
+    # Add a controller to reference the script (but never used). It is
+    # useful if the object will be imported from other blender file,
+    # inserting the script in the importer scene
+    bpy.ops.logic.controller_add(type='PYTHON',
+                                 name="{}.reference".format(SCRIPT_NAME),
+                                 object="")
+    text = None
+    for t in bpy.data.texts:
+        if t.name == '{}.py'.format(SCRIPT_NAME):
+            text = t
+            break
+    if text is None:
+        raise Exception('The script "{}.py is not loaded"'.format(SCRIPT_NAME))
+    obj.game.controllers[-1].mode = 'SCRIPT'
+    obj.game.controllers[-1].text = text
+
+    # Set the object as non-actor ghost object, i.e. it will detect collisions
+    # without either reacting or generating collisions.
+    obj.game.use_actor = False
+    obj.game.use_ghost = True
+
+
+def create():
+    dynamic.create()
+    destroyable.create()
+    generateProperties()
+    loadScript()
+    createLogic()
+
+
+def draw(context, layout):
+    dynamic.draw(context, layout)
+    destroyable.draw(context, layout)
+    row = layout.row()
+    row.prop(context.object,
+             "sss_min_angle",
+             text="Minimum angle (deg)")
+    row = layout.row()
+    row.prop(context.object,
+             "sss_max_angle",
+             text="Maximum angle (deg)")
+    row = layout.row()
+    row.prop(context.object,
+             "sss_vel_angle",
+             text="Rotation velocity (deg/s)")
+
---
+++ resources/sssSDK/scripts/sss_turret.py
@@ -0,0 +1,85 @@
+##############################################################################
+#                                                                            #
+#  This file is part of SonSilentSea, a free ships based combatr game.       #
+#  Copyright (C) 2014  Jose Luis Cercos Pita <jlcercos@gmail.com>            #
+#                                                                            #
+#  AQUAgpusph is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by      #
+#  the Free Software Foundation, either version 3 of the License, or         #
+#  (at your option) any later version.                                       #
+#                                                                            #
+#  AQUAgpusph is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#  GNU General Public License for more details.                              #
+#                                                                            #
+#  You should have received a copy of the GNU General Public License         #
+#  along with AQUAgpusph.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                            #
+##############################################################################
+
+import bge
+from bge import logic as g
+from math import *
+from mathutils import *
+from sss_dynamic import sssDynamic
+from sss_destroyable import sssDestroyable
+
+
+class sssTurret(sssDynamic, sssDestroyable):
+    def __init__(self, obj):
+        sssDynamic.__init__(self, obj)
+        sssDestroyable.__init__(self, obj)
+        self.min_yaw = radians(self['min_angle'])
+        self.max_yaw = radians(self['max_angle'])
+        self.vel_yaw = radians(self['vel_angle'])
+        self.aim = None
+
+    def aimTo(self, point):
+        self.aim = point.xy
+
+    def getDYaw(self):
+        if self.aim is None:
+            return 0.0
+        vec = self.aim - self.worldPosition.xy
+        vec = vec.normalized()
+        aim_yaw = atan2(vec.y, vec.x)
+        vec = self.getAxisVect(Vector((1.0, 0.0, 0.0))).xy
+        vec = vec.xy.normalized()
+        yaw = atan2(vec.y, vec.x)
+        return aim_yaw - yaw
+
+    def update(self):
+        sssDynamic.update(self)
+        sssDestroyable.update(self)
+        if self.aim is None:
+            return
+
+        dyaw = self.getDYaw()
+        yaw = self.localOrientation.to_euler().z
+        # Get the sortest way
+        if dyaw < -pi:
+            dyaw += 2.0 * pi
+        if dyaw > pi:
+            dyaw -= 2.0 * pi
+
+        # Test if we are trying to pass through the forbidden zone
+        # In that case we must change the rotation direction
+        if yaw + dyaw > self.max_yaw:
+            dyaw -= 2.0 * pi
+        if yaw + dyaw < self.min_yaw:
+            dyaw += 2.0 * pi
+
+        # Test if we are trying to aim out of the bounds
+        if yaw + dyaw > self.max_yaw or yaw + dyaw < self.min_yaw:
+            return
+
+        dt = 1.0 / g.getLogicTicRate()
+        max_rot = dt * self.vel_yaw
+        if abs(dyaw) > max_rot:
+            dyaw = copysign(max_rot, dyaw)
+        else:
+            # We will be already aimed at the end of this iteration
+            self.aim = None
+
+        self.applyRotation(Vector((0.0, 0.0, dyaw)), True)
